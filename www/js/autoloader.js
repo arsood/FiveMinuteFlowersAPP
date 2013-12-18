0b6155357220c21abee05cda8a76f19dee3e43e9
arsood <arsood@gmail.com>
2013-12-17 17:55:27 -0800

    Further work on autoloader

--- www/js/autoloader.js
+++ www/js/autoloader.js
@@ -2,6 +2,10 @@
 This document loads all pages and handles all of the buttloads of AJAX calls :P
 */
 
+localStorage.path_to_layouts = "http://localhost/sites/five_minute/page_layouts.php";
+localStorage.path_to_interface = "http://localhost/sites/five_minute/ajax_interface.php";
+localStorage.path_to_images = "C:/Users/arsoo_000/Documents/GitHub/FiveMinuteFlowersAPP/www/img/flowers/";
+
 //General loading functions
 
 function showLoader() {
@@ -23,7 +27,7 @@ $(document).on("pageshow", "#index", function() {
 });
 
 $(document).on("pagebeforeshow", "#index", function() {
-	$("#occasion-list").load("http://localhost/sites/five_minute/page_layouts.php?action=read&page-layout=index-categories", function() {
+	$("#occasion-list").load(localStorage.path_to_layouts + "?action=read&page-layout=index-categories", function() {
 		hideLoader();
 	});
 });
@@ -48,7 +52,7 @@ $(document).on("pageshow", "#browse", function() {
 
 $(document).on("pagebeforeshow", "#browse", function() {
 	$("#browse-page-title").html(localStorage.categoryChosen);
-	$("#browse-image-blocks").load("http://localhost/sites/five_minute/page_layouts.php?action=read&page-layout=browse-images&category=" + encodeURIComponent(localStorage.categoryChosen), function() {
+	$("#browse-image-blocks").load(localStorage.path_to_layouts + "?action=read&page-layout=browse-images&category=" + encodeURIComponent(localStorage.categoryChosen), function() {
 		hideLoader();
 	});
 });
@@ -60,14 +64,15 @@ $(document).on("pageshow", "#single", function() {
 });
 
 $(document).on("pagebeforeshow", "#single", function() {
-	$.get("http://localhost/sites/five_minute/ajax_interface.php?action=get-flower-info&arrangement=" + localStorage.specificFlower, function(data) {
+	$.get(localStorage.path_to_interface + "?action=get-flower-info&arrangement=" + localStorage.specificFlower, function(data) {
 		var flowerData = $.parseJSON(data);
 		var flowerImageCode = flowerData["arrangement_code"];
 		var flowerImage = flowerImageCode.replace("FLO", "flo");
 		$("#single-flower-name").html(flowerData["arrangement_name"]);
 		$("#single-flower-price").html("$" + flowerData["retail_price"]);
 		$("#single-flower-desc").html(flowerData["flower_description"]);
-		$("#single-flower-image").attr("src", "C:/Users/arsoo_000/Documents/GitHub/FiveMinuteFlowersAPP/www/img/flowers/"  + flowerImage + "_low.jpg");
+		$("#single-flower-image").attr("src", localStorage.path_to_images  + flowerImage + "_low.jpg");
+		$("#single-page-content").fadeIn();
 		hideLoader();
 	});
 });
@@ -79,7 +84,7 @@ function setPersonalizedOptions() {
 	localStorage.personalBudget = $("#personal-budget").val();
 	localStorage.personalOccasion = $("#personal-occasion").val();
 	localStorage.personalFlowerType = $("#personal-flower-type").val();
-	window.location = "#personalized";
+	$.mobile.changePage("#personalized", { transition: "flip" });
 }
 
 //Get personalized recommendations
@@ -89,7 +94,7 @@ $(document).on("pageshow", "#personalized", function() {
 });
 
 $(document).on("pagebeforeshow", "#personalized", function() {
-	$.post("http://localhost/sites/five_minute/ajax_interface.php", {
+	$.post(localStorage.path_to_interface, {
 		getAction: "get-personalized-array",
 		personalizedBudget: localStorage.personalBudget,
 		personalizedOccasion: localStorage.personalOccasion,
@@ -100,13 +105,17 @@ $(document).on("pagebeforeshow", "#personalized", function() {
 			var personalizedArray = $.parseJSON(localStorage.personalizedString);
 			localStorage.personalizedMaxArray = personalizedArray.length;
 			localStorage.personalizedCurrentIndex = 0;
-			$("#personalized-image-pic").attr("src", "C:/Users/arsoo_000/Documents/GitHub/FiveMinuteFlowersAPP/www/img/flowers/" + personalizedArray[0]["arrangement_code"].replace("FLO", "flo") + "_low.jpg");
+			$("#personalized-image-pic").attr("src", localStorage.path_to_images + personalizedArray[0]["arrangement_code"].replace("FLO", "flo") + "_low.jpg");
 			$("#personalized-image-name").html(personalizedArray[0]["arrangement_name"]);
 			$("#personalized-image-price").html("$" + personalizedArray[0]["retail_price"]);
 			$("#personalized-image-desc").html(personalizedArray[0]["flower_description"]);
+			
+			//Show the personalization page
+			
+			$("#personalized-page-content").fadeIn();
+			hideLoader();
+			renderPersonal();
 		}
-		hideLoader();
-		renderPersonal();
 	});
 });
 
@@ -120,7 +129,7 @@ $("#personalized-forward").click(function() {
 		var personalizedArray = $.parseJSON(localStorage.personalizedString);
 		localStorage.personalizedCurrentIndex = newIndex;
 		$(".ajax-block").fadeIn();
-		$("#personalized-image-pic").attr("src", "C:/Users/arsoo_000/Documents/GitHub/FiveMinuteFlowersAPP/www/img/flowers/" + personalizedArray[newIndex]["arrangement_code"].replace("FLO", "flo") + "_low.jpg");
+		$("#personalized-image-pic").attr("src", localStorage.path_to_images + personalizedArray[newIndex]["arrangement_code"].replace("FLO", "flo") + "_low.jpg");
 		$("#personalized-image-name").html(personalizedArray[newIndex]["arrangement_name"]);
 		$("#personalized-image-price").html("$" + personalizedArray[newIndex]["retail_price"]);
 		$("#personalized-image-desc").html(personalizedArray[newIndex]["flower_description"]);
@@ -136,7 +145,7 @@ $("#personalized-back").click(function() {
 		var personalizedArray = $.parseJSON(localStorage.personalizedString);
 		localStorage.personalizedCurrentIndex = newIndex;
 		$(".ajax-block").fadeIn();
-		$("#personalized-image-pic").attr("src", "C:/Users/arsoo_000/Documents/GitHub/FiveMinuteFlowersAPP/www/img/flowers/" + personalizedArray[newIndex]["arrangement_code"].replace("FLO", "flo") + "_low.jpg");
+		$("#personalized-image-pic").attr("src", localStorage.path_to_images + personalizedArray[newIndex]["arrangement_code"].replace("FLO", "flo") + "_low.jpg");
 		$("#personalized-image-name").html(personalizedArray[newIndex]["arrangement_name"]);
 		$("#personalized-image-price").html("$" + personalizedArray[newIndex]["retail_price"]);
 		$("#personalized-image-desc").html(personalizedArray[newIndex]["flower_description"]);
