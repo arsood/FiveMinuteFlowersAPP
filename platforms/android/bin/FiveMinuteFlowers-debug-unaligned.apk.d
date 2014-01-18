2eb7efcc0457d3660c6572bedb4b0ad67e382140
arsood <arsood@gmail.com>
2014-01-18 15:59:44 -0800

    Finalizing app

--- platforms/android/bin/FiveMinuteFlowers-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\bin\FiveMinuteFlowers-debug-unaligned.apk : \
-C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\bin\FiveMinuteFlowers.ap_ \
-C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\bin\classes.dex \
-C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\CordovaLib\bin\classes.jar \
---
+++ platforms/android/bin/FiveMinuteFlowers-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\bin\FiveMinuteFlowers-release-unsigned.apk : \
+C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\bin\FiveMinuteFlowers.ap_ \
+C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\bin\classes.dex \
+C:\Users\arsoo_000\Documents\GitHub\FiveMinuteFlowersAPP\platforms\android\CordovaLib\bin\classes.jar \
