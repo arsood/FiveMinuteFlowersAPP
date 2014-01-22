d095cfdc4598d57d65943271e6b03ced08c1d88e
arsood <arsood@gmail.com>
2014-01-22 12:12:06 -0800

    Finalizing for production

--- www/js/fmf.js
+++ www/js/fmf.js
@@ -13,7 +13,6 @@ var networkState = navigator.connection.type;
 
 if (networkState == "none") {
 	alert("Please connect to the internet to use this app :)");
-	return false;
 }
 
 //Check for kill code
